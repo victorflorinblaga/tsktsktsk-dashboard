fa5ba43927f5f2f2a26767480f0c4d0c739e3320
victorflorinblaga <115701667+victorflorinblaga@users.noreply.github.com>
2024-11-22 12:09:37 +0100

    updating contents of template/src/app/components/panel1.tsx

--- template/src/app/components/panel1.tsx
+++ template/src/app/components/panel1.tsx
@@ -1,12 +1,11 @@
+import { Textarea } from "@/components/ui/textarea";
 
-function panel1() {
-    return (
-      <div>
-        <h1>Panel 1</h1>
-      </div>
-    );
-  }
-  
-  export default panel1;
-  
-  
+const NotesPage = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <Textarea placeholder="Write your notes here..." className="w-1/2 h-1/2" />
+    </div>
+  );
+};
+
+export default NotesPage;
